416746c0b1c844bc619851fe9f592d470ea90cb7
bri <brigitta.barta@gmail.com>
2026-01-27 16:23:59 +0100

    ai[gemini]: How can I fetch data from GitHub if there are several subfolders? Request example, I adapted it using https://www.w3schools.com/PYTHON/ref_requests_response.asp

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,12 @@
 # import libraries
+import io
 from pathlib import Path
+import requests  # source/usage w3schools: https://www.w3schools.com/PYTHON/ref_requests_response.asp
 import cairosvg
+from PIL import Image
+
+# test url for one svg
+url = "https://raw.githubusercontent.com/duerrsimon/bioicons/main/static/icons/cc-0/Nucleic_acids/Kumar/DNA.svg"
 
 
 # config, folderstructure: static/svg (raw), static/png: img + caption(.txt.)
@@ -9,24 +15,31 @@ output_folder = Path("static/png")
 output_folder.mkdir(exist_ok=True)
 
 size = 1024 # to be compatible with FLUX.1-dev
+output_png = output_folder / "DNA.png"
 
 # find data, data located on github, loop through folders
 
 # download the .svgs in local folder
+response = requests.get(url)
+response.raise_for_status()
 
 # convert svgs into pngs(1024)
-for svg_file in input_folder.glob("*.svg"):
-    png_file=output_folder /svg_file.with_suffix(".png").name
+#for svg_file in input_folder.glob("*.svg"):
+#    png_file=output_folder /svg_file.with_suffix(".png").name
 
     # convert
-    cairosvg.svg2png(
-     url=str(svg_file),
-     write_to=str(png_file),
+png_data = cairosvg.svg2png(
+     bytestring=response.content,
+    # write_to=str(png_file),
      output_width=size,
-     output_height=size   
+     output_height=size,  
     )
 
-print(f"Test ok {png_file}")
+img = Image.open(io.BytesIO(png_data))
+img.save(output_png)
+
+
+print(f"Test ok {output_png}")
 
 # white background
 
