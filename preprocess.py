519b0c2e8468186202f135aff16c742012cdada5
bri <brigitta.barta@gmail.com>
2026-01-27 15:35:03 +0100

    added outline/plan

---
+++ preprocess.py
@@ -0,0 +1,15 @@
+# import libraries
+
+# config, folderstructure: static/svg (raw), static/png: img + caption(.txt.)
+
+# find data, data located on github, loop through folders
+
+# download the .svgs in local folder
+
+# convert svgs into pngs(1024)
+
+# white background
+
+# generate captions using the image names
+
+# save ds  
