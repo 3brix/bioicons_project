d65f56e9fcc5e1a7c740210a9f8f583504baa8b2
bri <brigitta.barta@gmail.com>
2026-02-02 19:51:16 +0100

    python script to upload dataset to modal, initial version

---
+++ upload_dataset.py
@@ -0,0 +1,35 @@
+#using: https://modal.com/docs/reference/modal.Volume#batch_upload
+
+from pathlib import Path
+import modal
+
+# define app
+app = modal.App("dataset-uploader")
+
+# define the volume
+volume = modal.Volume.from_name("bioicons-animals", create_if_missing=True)
+
+LOCAL_DATASET = Path("static/png")
+
+# @app.local_entrypoint() decorator
+@app.local_entrypoint()
+def main():
+    if not LOCAL_DATASET.exists():
+        print(f"Error: {LOCAL_DATASET} folder not found locally.")
+        return
+
+    print(f"Uploading files from {LOCAL_DATASET} to volume...")
+    
+    with volume.batch_upload() as batch:
+        count = 0
+        for file_path in LOCAL_DATASET.glob("*.*"):
+            if file_path.suffix in {".png", ".txt"}:
+                # put_file(local_path, remote_path)
+                batch.put_file(file_path, file_path.name)
+                count += 1
+        
+    print(f"Upload complete! {count} files uploaded to 'bioicons-animals'.")
+
+# CLI
+# run: modal run upload_dataset.py
+# sanity check: modal volume ls bioicons-animals
